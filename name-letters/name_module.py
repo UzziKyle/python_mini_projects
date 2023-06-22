3dc882595dae19367e1329e67fdcc03b46c975d2
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-06-22 11:08:20 +0800

    Update name_module.py

--- name-letters/name_module.py
+++ name-letters/name_module.py
@@ -18,17 +18,29 @@ class NameInfo():
         return self.full_name
 
     def get_all_letters(self) -> str:
+        '''
+        # I think ito ang pinaka-okay na way to create a list of all the letters in the full name. It's both short and readable.
         joined_names = "".join(self.full_name)
-
         letters = [letter.capitalize() for letter in joined_names if letter not in " '.-"]
+        '''
+
+        # This is the shortest you can write for it but is also the most difficult to read or understand
+        letters = [letter.capitalize() for name in self.full_name for letter in name if letter not in " '.-"] # R.I.P readability
+
+        '''
+        # Ganito siya if hindi gagamitan ng list comprehension
+        letters = []
+        for name in self.full_name:
+            for letter in name: 
+                if letter not in " '.-":
+                    letters.append(letter.capitalize())
+        '''
 
         filtered_letters = sorted(list(set(letters)))
         
-        # formatted_letters = [letter + ", " for letter in filtered_letters[:-1]]
-        # formatted_letters.append(filtered_letters[-1])
+        formatted_letters = [letter + ", " for letter in filtered_letters[:-1]]
+        formatted_letters.append(filtered_letters[-1])
 
-        # all_letters = "".join(formatted_letters)
+        all_letters = "".join(formatted_letters)
 
-        # return all_letters
-        
-        return filtered_letters
+        return all_letters
