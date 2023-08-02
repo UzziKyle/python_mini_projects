a66d2253602c1bb709cbf9fba733a4a53b77e3a3
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-08-02 11:29:05 +0800

    Update main.py

--- counter/main.py
+++ counter/main.py
@@ -2,16 +2,19 @@ import tkinter as tk
 
 # Functions
 def increase_value() -> None:
-    current_value = lbl_value['text']
-    lbl_value['text'] = int(current_value) + 1
+    current_value = int(lbl_value['text'])
+    if current_value == 999: return
+    lbl_value['text'] = current_value + 1
 
 def decrease_value() -> None:
-    current_value = lbl_value['text']
-    lbl_value['text'] = int(current_value) - 1
+    current_value = int(lbl_value['text'])
+    if current_value == 0: return
+    lbl_value['text'] = current_value - 1
 
 # Window
 window = tk.Tk()
 window.title('Counter App')
+window.resizable(width=0, height=0)
 
 # Widgets
 frm_main = tk.Frame(window, relief='sunken', borderwidth=5)
@@ -19,10 +22,10 @@ btn_decrease = tk.Button(frm_main, text='-', font=('Arial, 12'), width=3, comman
 btn_increase = tk.Button(frm_main, text='+', font=('Arial, 12'), width=3, command=increase_value)
 lbl_value = tk.Label(frm_main, text='0', font=('Arial, 16'), width=3)
 
-frm_main.grid(row=0, sticky='nsew')
-btn_decrease.grid(row=0, column=0, padx=5, pady=5, ipadx=5, ipady=5)
-btn_increase.grid(row=0, column=2, padx=5, pady=5, ipadx=5, ipady=5)
-lbl_value.grid(row=0, column=1, padx=10, pady=10, ipadx=5, ipady=5, sticky='ns')
+frm_main.grid(row=0, sticky='nsew', padx=20, pady=20)
+btn_decrease.grid(row=0, column=0, padx=15, ipadx=5, ipady=5)
+btn_increase.grid(row=0, column=2, padx=15, ipadx=5, ipady=5)
+lbl_value.grid(row=0, column=1, pady=15, sticky='ns')
 
 # Execution
 window.mainloop()
