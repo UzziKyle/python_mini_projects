ca64fee8932d4b17ecc50072c34044616b9d760e
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-08-04 16:58:22 +0800

    refactor into OOP

--- simple-dice-app/main.py
+++ simple-dice-app/main.py
@@ -1,26 +1,28 @@
 import tkinter as tk
 from random import randint
 
-# Functions
-def roll() -> None:
-    lbl_result['text'] = str(randint(1,6))
+class App(tk.Tk):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title('Dice App')
+        self.resizable(width=0, height=0)
 
-# Window
-window = tk.Tk()
-window.title('Dice App')
-window.resizable(width=0, height=0)
+        # Widgets
+        frm_result = tk.Frame(self, relief='sunken', borderwidth=4)
+        lbl_result = tk.Label(frm_result, font=('Arial, 16'), width=15, pady=20)
+        frm_roll = tk.Frame(self, relief='flat', borderwidth=2)
+        btn_roll = tk.Button(frm_roll, text='Roll', font=('Arial, 12'), width=10, command=lambda: roll())
 
-# Widgets
-frm_result = tk.Frame(window, relief='sunken', borderwidth=4)
-lbl_result = tk.Label(frm_result, font=('Arial, 16'), width=17, pady=20)
-frm_roll = tk.Frame(window, relief='flat', borderwidth=2)
-btn_roll = tk.Button(frm_roll, text='Roll', font=('Arial, 12'), width=17, command=roll)
+        # Widgets Position
+        frm_result.grid(row=0, padx=5, pady=1, sticky='n')
+        lbl_result.grid(row=0, sticky='ns')
+        frm_roll.grid(row=1, padx=5, pady=3, sticky='ew')
+        btn_roll.pack() # Use .pack() when centering widgets... 
 
-# Widgets Position
-frm_result.grid(row=0, padx=4, pady=1, sticky='n')
-lbl_result.grid(row=0, sticky='ns')
-frm_roll.grid(row=1, padx=6, pady=3, sticky='ew')
-btn_roll.pack() # Use .pack() when centering widgets... 
+        # Functions
+        def roll() -> None:
+            lbl_result['text'] = str(randint(1,6))
 
-# Execute
-window.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
