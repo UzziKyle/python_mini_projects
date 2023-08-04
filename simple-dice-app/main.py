8d07bb2618db2c71c6bd8f548effeb489b4a3761
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-08-04 17:59:36 +0800

    refactor roll()

--- simple-dice-app/main.py
+++ simple-dice-app/main.py
@@ -11,7 +11,7 @@ class App(tk.Tk):
         frm_result = tk.Frame(self, relief='sunken', borderwidth=4)
         lbl_result = tk.Label(frm_result, font=('Arial, 16'), width=15, pady=20)
         frm_roll = tk.Frame(self, relief='flat', borderwidth=2)
-        btn_roll = tk.Button(frm_roll, text='Roll', font=('Arial, 12'), width=10, command=lambda: roll())
+        btn_roll = tk.Button(frm_roll, text='Roll', font=('Arial, 12'), width=10, command=lambda: self.roll(lbl_result))
 
         # Widgets Position
         frm_result.grid(row=0, padx=5, pady=1, sticky='n')
@@ -19,9 +19,9 @@ class App(tk.Tk):
         frm_roll.grid(row=1, padx=5, pady=3, sticky='ew')
         btn_roll.pack() # Use .pack() when centering widgets... 
 
-        # Functions
-        def roll() -> None:
-            lbl_result['text'] = str(randint(1,6))
+    # Functions
+    def roll(self, lbl_result) -> None:
+        lbl_result['text'] = str(randint(1,6))
 
 if __name__ == "__main__":
     app = App()
