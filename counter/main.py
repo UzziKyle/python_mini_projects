44c6d0499f88cc6bf45d78a5d8319957782c21bb
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-08-05 05:20:55 +0800

    refactor into OOP

--- counter/main.py
+++ counter/main.py
@@ -1,32 +1,41 @@
 import tkinter as tk
 
-# Functions
-def increase_value() -> None:
-    current_value = int(lbl_value['text'])
-    if current_value == 999: return
-    lbl_value['text'] = current_value + 1
+class Window(tk.Tk):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title('Counter App')
+        self.resizable(False, False)
 
-def decrease_value() -> None:
-    current_value = int(lbl_value['text'])
-    if current_value == 0: return
-    lbl_value['text'] = current_value - 1
+class MainFrame(tk.Frame):
+    def __init__(self, master) -> None:
+        super().__init__()
+        self.master = master
+        self.relief = 'sunken'
+        self.borderwidth = 5
+        self.grid(row=0, sticky='nsew', padx=20, pady=20)
 
-# Window
-window = tk.Tk()
-window.title('Counter App')
-window.resizable(width=0, height=0)
+        # Widgets
+        btn_decrease = tk.Button(self, text='-', font=('Arial, 12'), width=3, command=lambda: self.decrease_value(lbl_value))
+        btn_increase = tk.Button(self, text='+', font=('Arial, 12'), width=3, command=lambda: self.increase_value(lbl_value))
+        lbl_value = tk.Label(self, text='0', font=('Arial, 16'), width=3)
 
-# Widgets
-frm_main = tk.Frame(window, relief='sunken', borderwidth=5)
-btn_decrease = tk.Button(frm_main, text='-', font=('Arial, 12'), width=3, command=decrease_value)
-btn_increase = tk.Button(frm_main, text='+', font=('Arial, 12'), width=3, command=increase_value)
-lbl_value = tk.Label(frm_main, text='0', font=('Arial, 16'), width=3)
+        # Widget Positions
+        btn_decrease.grid(row=0, column=0, padx=15, ipadx=5, ipady=5)
+        btn_increase.grid(row=0, column=2, padx=15, ipadx=5, ipady=5)
+        lbl_value.grid(row=0, column=1, pady=10, sticky='ns')
 
-# Widget Positions
-frm_main.grid(row=0, sticky='nsew', padx=20, pady=20)
-btn_decrease.grid(row=0, column=0, padx=15, ipadx=5, ipady=5)
-btn_increase.grid(row=0, column=2, padx=15, ipadx=5, ipady=5)
-lbl_value.grid(row=0, column=1, pady=15, sticky='ns')
+    # Functions
+    def increase_value(self, lbl_value) -> None:
+        current_value = int(lbl_value['text'])
+        if current_value == 999: return
+        lbl_value['text'] = current_value + 1
 
-# Execution
-window.mainloop()
+    def decrease_value(self, lbl_value) -> None:
+        current_value = int(lbl_value['text'])
+        if current_value == 0: return
+        lbl_value['text'] = current_value - 1
+
+if __name__ == '__main__':
+    window = Window()
+    main_frame = MainFrame(window)
+    window.mainloop()
