ab9d1de24031a2cb5b8692893a55c8be652c0435
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-10-23 21:29:57 +0800

    Add files via upload

---
+++ number-guessing-game/main.py
@@ -0,0 +1,58 @@
+class GuessingGame:
+    def __init__(self, answer: int, min: int = 0, max: int = 30) -> None:
+        self._answer = answer
+        self._min = min
+        self._max = max
+        self._guess = 0
+        
+    def reset_guess_count(self) -> None:
+        self._guess = 0
+        
+    def get_guess(self) -> int:
+        self._guess += 1
+        
+        guess = input(f'Guess the number between {self._min} - {self._max}: ')
+        
+        valid, answer = self.validate_answer(guess)
+        
+        if not valid:
+            print('Invalid Answer!')
+            return self.get_guess()
+        
+        return answer 
+    
+    def validate_answer(self, answer: int) -> (bool, int):
+        try:
+            num = int(answer)
+            
+        except:
+            return False, -1
+            
+        return self._min <= num <= self._max, num
+        
+    def play(self) -> None:
+        self.reset_guess_count()
+        
+        while True:            
+            answer = self.get_guess()
+            
+            if answer > self._answer:
+                print('Your guess was over.')
+                
+            elif answer < self._answer:
+                print('Your guess was under.')
+                
+            else:
+                break
+        
+        print(f'You got the answer within {self._guess} guess.')
+        
+        
+def main() -> None:
+    game = GuessingGame(12)
+    
+    game.play()
+    
+    
+if __name__ == '__main__':
+    main()
